6f30732986f71cc49eaf477cb651f027e6310ad1
Samson <sfagade@gmail.com>
2020-11-16 15:26:40 +0100

    Started project. Before entity mappings

---
+++ src/main/java/com/app/storytel/challenge/StorytelMessagesApplication.java
@@ -0,0 +1,13 @@
+package com.app.storytel.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorytelMessagesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StorytelMessagesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/storytel/challenge/StorytelMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.storytel.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorytelMessagesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
